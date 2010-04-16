8b38930da1ec137713a9aa2bd5dfec859b9d6f14
byeo <byeo@angel>
2010-04-17 00:39:06 +0100

    Radio Button UI - Makes it more obvious that phone dialing is disabled.

--- DroidDialer/src/com/xpdeveloper/dialer/DroidDialer.java
+++ DroidDialer/src/com/xpdeveloper/dialer/DroidDialer.java
@@ -20,9 +20,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.provider.ContactsContract.Contacts;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 /**
  * 
@@ -31,28 +33,44 @@ import android.widget.CompoundButton.OnCheckedChangeListener;
  */
 public class DroidDialer extends Activity {
 	private static final String PREF_ENABLE_TONES = "enableTones";
-	private static boolean _tonesEnabled = false; //TODO move this into a service
+	private static boolean _tonesEnabled = false; // TODO move this into a
+													// service
 	public static String PREFS_NAME = "DroidDailer";
+	private RadioGroup _enableGroup;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		CheckBox enableTonesCheckBox = (CheckBox)findViewById(R.id.EnableTonesCheckBox);
-		enableTonesCheckBox.setEnabled(getTonesEnabled());
-		enableTonesCheckBox.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+		_enableGroup = (RadioGroup) findViewById(R.id.RadioGroupEnable);
+		_enableGroup.check(checkedButtonId(getTonesEnabled()));
+		_enableGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
 			@Override
-			public void onCheckedChanged(CompoundButton view, boolean isChecked) {
-				setTonesEnabled(isChecked);
+			public void onCheckedChanged(RadioGroup arg0, int checkedId) {
+				if (checkedId == R.id.RadioButtonTone) {
+					setTonesEnabled(true);
+				}
+				setTonesEnabled(false);
+			}
+		});
+
+		Button contactsButton = (Button) findViewById(R.id.button_contacts);
+		contactsButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent(Intent.ACTION_VIEW, Contacts.CONTENT_URI);
+				startActivity(intent);
 			}
 		});
 	}
 
-	private void launchContactChooser() {
-		// Launch the contacts browser
-		Intent intent = new Intent(Intent.ACTION_VIEW, Contacts.CONTENT_URI);
-		startActivity(intent);
+	private int checkedButtonId(boolean tonesEnabled) {
+		if (tonesEnabled) {
+			return R.id.RadioButtonTone;
+		}
+		return R.id.RadioButtonPhone;
 	}
 
 	@Override
@@ -72,7 +90,7 @@ public class DroidDialer extends Activity {
 		editor.putBoolean(PREF_ENABLE_TONES, enableTones);
 		editor.commit();
 	}
-	
+
 	public boolean getTonesEnabled() {
 		SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
 		_tonesEnabled = settings.getBoolean(PREF_ENABLE_TONES, true);
