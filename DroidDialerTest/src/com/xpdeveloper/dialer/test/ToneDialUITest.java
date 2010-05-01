59580e7b5cf8a13ebfe2a59dd9e818a1979f0ccb
Oliver Bye <byeo@angel.(none)>
2010-05-01 23:50:02 +0100

    Add Robotium tests

---
+++ DroidDialerTest/src/com/xpdeveloper/dialer/test/ToneDialUITest.java
@@ -0,0 +1,59 @@
+package com.xpdeveloper.dialer.test;
+
+import java.util.ArrayList;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.jayway.android.robotium.solo.Solo;
+import com.xpdeveloper.dialer.R;
+import com.xpdeveloper.dialer.ToneDialActivity;
+
+public class ToneDialUITest extends
+		ActivityInstrumentationTestCase2<ToneDialActivity> {
+
+	private Solo _solo;
+
+	/**
+	 * Tests require a default constructor
+	 */
+	public ToneDialUITest() {
+		super(ToneDialActivity.class.getPackage().getName(),
+				ToneDialActivity.class);
+	}
+
+	@Override
+	public void setUp() throws Exception {
+		_solo = new Solo(getInstrumentation(), getActivity());
+	}
+
+	@Override
+	public void tearDown() throws Exception {
+
+		try {
+			_solo.finalize();
+		} catch (Throwable e) {
+
+			e.printStackTrace();
+		}
+		getActivity().finish();
+		super.tearDown();
+
+	}
+
+	public void testToneDial() {
+		ToneDialActivity unit = getActivity();
+		
+		assertTrue("Expecting the Tone Dial Page", _solo
+				.searchText("Tone Dial"));
+
+		ArrayList<Button> buttons = _solo.getCurrentButtons();
+		ArrayList<ListView> lists = _solo.getCurrentListViews();
+		ListView preferences = lists.get(0);
+		View enableRow = preferences.getChildAt(0);
+		_solo.clickOnScreen(enableRow);
+	}
+
+}
