cbe122d7b33e208ce0197adb768e98c46e32c25c
Oliver Bye <byeo@angel.(none)>
2010-05-04 22:14:18 +0100

    Get the number tweak passing (Pass) - TODO a list of swaps does not make good preferences - TODO consider simple Country and STD code

--- DroidDialer/src/net/xpdeveloper/dialer/NumberTweaker.java
+++ DroidDialer/src/net/xpdeveloper/dialer/NumberTweaker.java
@@ -5,17 +5,26 @@ import java.util.Map;
 import java.util.Set;
 
 public class NumberTweaker {
-	private Map<String, String> _swaps = new HashMap<String, String>(); 
-	
+	private Map<String, String> _swaps = new HashMap<String, String>();
+
 	/**
-	 * Add a swap 
-	 * @param prefix what to search for
-	 * @param newPrefix what to swap it for
+	 * Add a swap
+	 * 
+	 * @param prefix
+	 *            what to search for
+	 * @param newPrefix
+	 *            what to swap it for
 	 */
 	public void addSwap(String prefix, String newPrefix) {
 		_swaps.put(prefix, newPrefix);
 	}
 
+	/**
+	 * If the numberString starts with any of the swaps then swap it
+	 * 
+	 * @param numberString
+	 * @return
+	 */
 	public String tweak(String numberString) {
 		String reply = numberString;
 		Set<String> keys = _swaps.keySet();
